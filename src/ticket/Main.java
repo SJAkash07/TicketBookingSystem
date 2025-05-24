0b278a856062b54e199481dacc524dccc2b4d5ce
SJAkash07 <suresh.24scse1420027@galgotiasuniversity.ac.in>
2025-05-24 10:34:33 +0530

    Create Main.java

---
+++ src/ticket/Main.java
@@ -0,0 +1,10 @@
+package com.ticket;
+
+import com.ticket.ui.AuthScreen;
+
+public class Main {
+    public static void main(String[] args) {
+        // Launch the authentication screen
+        new AuthScreen();
+    }
+}
