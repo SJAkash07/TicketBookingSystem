82eec80d53bef57594aa32d4adb2cdbd26455396
SJAkash07 <suresh.24scse1420027@galgotiasuniversity.ac.in>
2025-06-07 16:54:52 +0530

    Update BookinDAO.java

--- src/dao/BookinDAO.java
+++ src/dao/BookinDAO.java
@@ -1,4 +1,4 @@
-package com.ticket.dao;
+package src.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.ticket.module.Booking;
+import src.model.Booking;
 
 public class BookingDAO {
 
